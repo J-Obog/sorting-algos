ff786915cfc4b4748d66975cb09fa6d20ea88508
kgerrato <90009972+kgerrato@users.noreply.github.com>
2022-04-03 19:27:36 -0400

    Added signatures selection, insertion and bubble

--- algos.hpp
+++ algos.hpp
@@ -1,2 +1,5 @@
 void merge(int arr[], int i, int j, int k);
 void mergeSort(int arr[], int i, int k);
+void selectionSort(int arr[], int n);
+void insertionSort(int arr[], int n);
+void bubbleSort(int arr[], int n)
